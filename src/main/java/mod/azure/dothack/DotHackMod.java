cc698625f4faa8782dbecd7365e1c4f4cc3be63a
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-08-24 03:12:18 -0400

    v.0.0.4

--- src/main/java/mod/azure/dothack/DotHackMod.java
+++ src/main/java/mod/azure/dothack/DotHackMod.java
@@ -8,6 +8,11 @@ import mod.azure.dothack.items.DotHackItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
@@ -16,73 +21,77 @@ public class DotHackMod implements ModInitializer {
 	DotHackMobs MOBS;
 	DotHackItems ITEMS;
 	public static final String MODID = "dothack";
-
-	public static final CreativeModeTab NPC_TAB = FabricItemGroup.builder(DotHackMod.modResource("entities")).icon(() -> new ItemStack(DotHackItems.AZUREKITE_SPAWN_EGG)).displayItems((context, entries) -> {
-		entries.accept(DotHackItems.AZUREKITE_SPAWN_EGG);
-		entries.accept(DotHackItems.ACEROLA_SPAWN_EGG);
-		entries.accept(DotHackItems.AINA_SPAWN_EGG);
-		entries.accept(DotHackItems.ALKAID_SPAWN_EGG);
-		entries.accept(DotHackItems.ATOLI_SPAWN_EGG);
-		entries.accept(DotHackItems.AURA_SPAWN_EGG);
-		entries.accept(DotHackItems.AZUREBALMUNG_SPAWN_EGG);
-		entries.accept(DotHackItems.AZUREORCA_SPAWN_EGG);
-		entries.accept(DotHackItems.BALMUNG_SPAWN_EGG);
-		entries.accept(DotHackItems.BEAR_SPAWN_EGG);
-		entries.accept(DotHackItems.BLACKROSE_SPAWN_EGG);
-		entries.accept(DotHackItems.BLADEMASTER_SPAWN_EGG);
-		entries.accept(DotHackItems.BT_SPAWN_EGG);
-		entries.accept(DotHackItems.CREST_SPAWN_EGG);
-		entries.accept(DotHackItems.CRIM_SPAWN_EGG);
-		entries.accept(DotHackItems.ELK_SPAWN_EGG);
-		entries.accept(DotHackItems.FLARE_SPAWN_EGG);
-		entries.accept(DotHackItems.HASEO_SPAWN_EGG);
-		entries.accept(DotHackItems.HASEOXTH_SPAWN_EGG);
-		entries.accept(DotHackItems.HEAVYAXEMAN_SPAWN_EGG);
-		entries.accept(DotHackItems.HELBA_SPAWN_EGG);
-		entries.accept(DotHackItems.KITE_SPAWN_EGG);
-		entries.accept(DotHackItems.KNIGHT_SPAWN_EGG);
-		entries.accept(DotHackItems.KOJI_SPAWN_EGG);
-		entries.accept(DotHackItems.KOMIYANIII_SPAWN_EGG);
-		entries.accept(DotHackItems.KUHN_SPAWN_EGG);
-		entries.accept(DotHackItems.LOIS_SPAWN_EGG);
-		entries.accept(DotHackItems.LONGARMM_SPAWN_EGG);
-		entries.accept(DotHackItems.MIA_SPAWN_EGG);
-		entries.accept(DotHackItems.MICINO_SPAWN_EGG);
-		entries.accept(DotHackItems.MISTRAL_SPAWN_EGG);
-		entries.accept(DotHackItems.OBOROZUKIYO_SPAWN_EGG);
-		entries.accept(DotHackItems.ORCA_SPAWN_EGG);
-		entries.accept(DotHackItems.OKUA_SPAWN_EGG);
-		entries.accept(DotHackItems.OVAN_SPAWN_EGG);
-		entries.accept(DotHackItems.PANTA_SPAWN_EGG);
-		entries.accept(DotHackItems.PI_SPAWN_EGG);
-		entries.accept(DotHackItems.SAKUYA_SPAWN_EGG);
-		entries.accept(DotHackItems.SILBABUS_SPAWN_EGG);
-		entries.accept(DotHackItems.SORA_SPAWN_EGG);
-		entries.accept(DotHackItems.SUBARU_SPAWN_EGG);
-		entries.accept(DotHackItems.TABBY_SPAWN_EGG);
-		entries.accept(DotHackItems.TASK_SPAWN_EGG);
-		entries.accept(DotHackItems.TWINBLADE_SPAWN_EGG);
-		entries.accept(DotHackItems.TWINBLADEF_SPAWN_EGG);
-		entries.accept(DotHackItems.WAVEMASTERF_SPAWN_EGG);
-		entries.accept(DotHackItems.ZELKOVA_SPAWN_EGG);
-		entries.accept(DotHackItems.CHIMCHIM_SPAWN_EGG);
-		entries.accept(DotHackItems.SKEITH_SPAWN_EGG);
-	}).build();
-
-	public static final CreativeModeTab ITEM_TAB = FabricItemGroup.builder(DotHackMod.modResource("items")).icon(() -> new ItemStack(DotHackItems.ULTIMATESTEEL)).displayItems((context, entries) -> {
-		entries.accept(DotHackItems.FLAMESWORD);
-		entries.accept(DotHackItems.GOLDENSPEAR);
-		entries.accept(DotHackItems.SPINGAIGU);
-		entries.accept(DotHackItems.DORJEHETCHETA);
-		entries.accept(DotHackItems.SPINDRAKE);
-		entries.accept(DotHackItems.SPINRIBS);
-		entries.accept(DotHackItems.ULTIMATESTEEL);
-	}).build();
+	public static final ResourceKey<CreativeModeTab> NPC_TAB = ResourceKey.create(Registries.CREATIVE_MODE_TAB, DotHackMod.modResource("entities"));
+	public static final ResourceKey<CreativeModeTab> ITEM_TAB = ResourceKey.create(Registries.CREATIVE_MODE_TAB, DotHackMod.modResource("items"));
 
 	@Override
 	public void onInitialize() {
 		MOBS = new DotHackMobs();
 		ITEMS = new DotHackItems();
+		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, NPC_TAB, FabricItemGroup.builder().icon(() -> new ItemStack(DotHackItems.AZUREKITE_SPAWN_EGG)) // icon
+				.title(Component.translatable("itemGroup.dothack.entities")) // title
+				.displayItems((context, entries) -> {
+					entries.accept(DotHackItems.AZUREKITE_SPAWN_EGG);
+					entries.accept(DotHackItems.ACEROLA_SPAWN_EGG);
+					entries.accept(DotHackItems.AINA_SPAWN_EGG);
+					entries.accept(DotHackItems.ALKAID_SPAWN_EGG);
+					entries.accept(DotHackItems.ATOLI_SPAWN_EGG);
+					entries.accept(DotHackItems.AURA_SPAWN_EGG);
+					entries.accept(DotHackItems.AZUREBALMUNG_SPAWN_EGG);
+					entries.accept(DotHackItems.AZUREORCA_SPAWN_EGG);
+					entries.accept(DotHackItems.BALMUNG_SPAWN_EGG);
+					entries.accept(DotHackItems.BEAR_SPAWN_EGG);
+					entries.accept(DotHackItems.BLACKROSE_SPAWN_EGG);
+					entries.accept(DotHackItems.BLADEMASTER_SPAWN_EGG);
+					entries.accept(DotHackItems.BT_SPAWN_EGG);
+					entries.accept(DotHackItems.CREST_SPAWN_EGG);
+					entries.accept(DotHackItems.CRIM_SPAWN_EGG);
+					entries.accept(DotHackItems.ELK_SPAWN_EGG);
+					entries.accept(DotHackItems.FLARE_SPAWN_EGG);
+					entries.accept(DotHackItems.HASEO_SPAWN_EGG);
+					entries.accept(DotHackItems.HASEOXTH_SPAWN_EGG);
+					entries.accept(DotHackItems.HEAVYAXEMAN_SPAWN_EGG);
+					entries.accept(DotHackItems.HELBA_SPAWN_EGG);
+					entries.accept(DotHackItems.KITE_SPAWN_EGG);
+					entries.accept(DotHackItems.KNIGHT_SPAWN_EGG);
+					entries.accept(DotHackItems.KOJI_SPAWN_EGG);
+					entries.accept(DotHackItems.KOMIYANIII_SPAWN_EGG);
+					entries.accept(DotHackItems.KUHN_SPAWN_EGG);
+					entries.accept(DotHackItems.LOIS_SPAWN_EGG);
+					entries.accept(DotHackItems.LONGARMM_SPAWN_EGG);
+					entries.accept(DotHackItems.MIA_SPAWN_EGG);
+					entries.accept(DotHackItems.MICINO_SPAWN_EGG);
+					entries.accept(DotHackItems.MISTRAL_SPAWN_EGG);
+					entries.accept(DotHackItems.OBOROZUKIYO_SPAWN_EGG);
+					entries.accept(DotHackItems.ORCA_SPAWN_EGG);
+					entries.accept(DotHackItems.OKUA_SPAWN_EGG);
+					entries.accept(DotHackItems.OVAN_SPAWN_EGG);
+					entries.accept(DotHackItems.PANTA_SPAWN_EGG);
+					entries.accept(DotHackItems.PI_SPAWN_EGG);
+					entries.accept(DotHackItems.SAKUYA_SPAWN_EGG);
+					entries.accept(DotHackItems.SILBABUS_SPAWN_EGG);
+					entries.accept(DotHackItems.SORA_SPAWN_EGG);
+					entries.accept(DotHackItems.SUBARU_SPAWN_EGG);
+					entries.accept(DotHackItems.TABBY_SPAWN_EGG);
+					entries.accept(DotHackItems.TASK_SPAWN_EGG);
+					entries.accept(DotHackItems.TWINBLADE_SPAWN_EGG);
+					entries.accept(DotHackItems.TWINBLADEF_SPAWN_EGG);
+					entries.accept(DotHackItems.WAVEMASTERF_SPAWN_EGG);
+					entries.accept(DotHackItems.ZELKOVA_SPAWN_EGG);
+					entries.accept(DotHackItems.CHIMCHIM_SPAWN_EGG);
+					entries.accept(DotHackItems.SKEITH_SPAWN_EGG);
+				}).build()); // build() no longer registers by itself
+		Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, ITEM_TAB, FabricItemGroup.builder().icon(() -> new ItemStack(DotHackItems.ULTIMATESTEEL)) // icon
+				.title(Component.translatable("itemGroup.dothack.items")) // title
+				.displayItems((context, entries) -> {
+					entries.accept(DotHackItems.FLAMESWORD);
+					entries.accept(DotHackItems.GOLDENSPEAR);
+					entries.accept(DotHackItems.SPINGAIGU);
+					entries.accept(DotHackItems.DORJEHETCHETA);
+					entries.accept(DotHackItems.SPINDRAKE);
+					entries.accept(DotHackItems.SPINRIBS);
+					entries.accept(DotHackItems.ULTIMATESTEEL);
+				}).build()); // build() no longer registers by itself
 		FabricDefaultAttributeRegistry.register(DotHackMobs.AZUREKITE, NPCEntity.createMobAttributes());
 		FabricDefaultAttributeRegistry.register(DotHackMobs.ACEROLA, NPCEntity.createMobAttributes());
 		FabricDefaultAttributeRegistry.register(DotHackMobs.AINA, NPCEntity.createMobAttributes());
